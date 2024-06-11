6da3ccd85a77d7df2b2b2e03e34a21343e639e32
MuStaFa KhaShaBa <moustafa_165736@fci.sohag.edu.eg>
2024-06-12 02:49:28 +0300

    some enhancments

--- Medical/Program.cs
+++ Medical/Program.cs
@@ -19,7 +19,7 @@ namespace Medical
 
             var app = builder.Build();
 
-           await app.AddAppConfig();
+            await app.AddAppConfig();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
